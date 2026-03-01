a56d47d4cdca43551335415ceeb8dd42374cb385
sam <22.samarpan@gmail.com>
2026-03-02 00:03:26 +0530

    new theame

--- js/damage.js
+++ js/damage.js
@@ -3,17 +3,20 @@ export function createDamageSystem(app) {
     let damageSpots = [];
 
     function create(x, y) {
+        const texture = PIXI.Texture.from('assets/damage.png');
+        const crack = new PIXI.Sprite(texture);
 
-        const hole = new PIXI.Graphics();
-        hole.beginFill(0x000000);
-        hole.drawCircle(0, 0, 20);
-        hole.endFill();
+        crack.anchor.set(0.5);
+        crack.x = x;
+        crack.y = y;
 
-        hole.x = x;
-        hole.y = y;
+        // Randomize scale and rotation for variety
+        crack.scale.set(0.1 + Math.random() * 0.1);
+        crack.rotation = Math.random() * Math.PI * 2;
+        crack.alpha = 0.8;
 
-        app.stage.addChild(hole);
-        damageSpots.push(hole);
+        app.stage.addChild(crack);
+        damageSpots.push(crack);
     }
 
     function getDamage() {
